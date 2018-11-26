41561e5d461eec1bfdff28d12e266f795c26e3b1
Miguel Baril-Lahaie <barm1815@usherbrooke.ca>
2018-11-26 12:13:53 -0500

    fonction start

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -2,7 +2,7 @@
 Simple udp client
 Silver Moon (m00n.silv3r@gmail.com)
 */
-
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <iostream>
 #include <winsock2.h>
 #include <fstream>
@@ -23,51 +23,27 @@ miguelito libre
 #define BUFLEN 512  //Max length of buffer
 #define PORT 8888   //The port on which to listen for incoming data
 
+NetworkServices TransportLayer;
+
 int main(void)
 {
-	struct sockaddr_in si_other;
-	int s, slen = sizeof(si_other);
-	char buf[BUFLEN];
-	char message[BUFLEN];
-	WSADATA wsa;
-
-	//Initialise winsock
-	printf("\nInitialising Winsock...");
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-	{
-		printf("Failed. Error Code : %d", WSAGetLastError());
-		exit(EXIT_FAILURE);
-	}
-	printf("Initialised.\n");
-
-	//create socket
-	if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR)
-	{
-		printf("socket() failed with error code : %d", WSAGetLastError());
-		exit(EXIT_FAILURE);
-	}
-
-	//setup address structure
-	memset((char *)&si_other, 0, sizeof(si_other));
-	si_other.sin_family = AF_INET;
-	si_other.sin_port = htons(PORT);
-	si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
 
 	// Fonction pour aller lire dans fichier text + adresse destinataire 
 	ifstream inFile;
 	string FileName;
 	string ipAdress;
 	const int File_Length = 8192;
-	char FileContent[File_Length];
-
-	
+	//char FileContent[File_Length];
 
 
 	//start communication
 	while (1)
 	{
 		
-		printf("\n Entrez le nom du fichier complet à téléverser : ");
+		TransportLayer.start();
+
+
+		/*printf("\n Entrez le nom du fichier complet à téléverser : ");
 		cin >> FileName;
 
 		if (FileName == "validation.txt") {
@@ -90,17 +66,17 @@ int main(void)
 		printf("\n Entrer ladresse ip du destinataire (127.0.0.1) ");
 		cin >> ipAdress;
 
-		sendFile(FileContent, File_Length);
+		TransportLayer.sendFile(FileContent, File_Length);
 
 		//printf("\n Entrez le message : ");
 		//cin >> message;
 
 		//send the message
-		/*if (sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
+		if (sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
 		{
 			printf("sendto() failed with error code : %d", WSAGetLastError());
 			exit(EXIT_FAILURE);
-		}*/
+		}
 
 		//receive a reply and print it
 		//clear the buffer by filling null, it might have previously received data
@@ -115,8 +91,7 @@ int main(void)
 		cout << buf;*/
 	}
 
-	closesocket(s);
-	WSACleanup();
+
 
 	return 0;
 }
--- NetworkServices/NetworkServices.cpp
+++ NetworkServices/NetworkServices.cpp
@@ -1,6 +1,22 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include "NetworkServices.h"
+#include <iostream>
+#include <winsock2.h>
+#include <fstream>
+#include <string>
 
 using namespace std;
+
+#define SERVER "127.0.0.1"  //ip address of udp server
+#define BUFLEN 512  //Max length of buffer
+#define PORT 8888   //The port on which to listen for incoming data
+
+struct sockaddr adress;
+struct sockaddr_in si_other;
+int soc, slen = sizeof(si_other);
+char buf[BUFLEN];
+char message[BUFLEN];
+WSADATA wsa;
 
 NetworkServices::NetworkServices()
 {
@@ -13,6 +29,12 @@ NetworkServices::NetworkServices()
 	{
 		throw runtime_error("Failed to create socket");
 	}
+
+	//setup address structure
+	memset((char *)&si_other, 0, sizeof(si_other));
+	si_other.sin_family = AF_INET;
+	si_other.sin_port = htons(PORT);
+	si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
 }
 
 
@@ -22,8 +44,21 @@ NetworkServices::~NetworkServices()
 	WSACleanup();
 }
 
-bool NetworkServices::start(char * ipAddress, unsigned int port)
+bool NetworkServices::start()
 {
+	int test;
+	int test2;
+	int connection_succes = -1;
+	connection_succes = connect(soc, (struct sockaddr *) &si_other, sizeof(si_other));
+	printf("after conenct()\n");  //for debugging
+	if (connection_succes != 0)
+	{
+		printf("Error %d in client while instantiating socket\n", WSAGetLastError());
+		cin >> test2;
+	}
+	printf("The client has found a server!");
+
+	cin >> test;
 
 	return true;
 }
