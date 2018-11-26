0c94fabc99e76955f888a8db96b9a0fa2db6d836
Miguel Baril-Lahaie <barm1815@usherbrooke.ca>
2018-11-25 23:34:28 -0500

    allo

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -5,6 +5,9 @@ Silver Moon (m00n.silv3r@gmail.com)
 
 #include <iostream>
 #include <winsock2.h>
+#include <fstream>
+#include <string>
+#include "../NetworkServices/NetworkServices.h"
 
 using namespace std;
 
@@ -50,30 +53,66 @@ int main(void)
 	si_other.sin_port = htons(PORT);
 	si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
 
+	// Fonction pour aller lire dans fichier text + adresse destinataire 
+	ifstream inFile;
+	string FileName;
+	string ipAdress;
+	const int File_Length = 8192;
+	char FileContent[File_Length];
+
+	
+
+
 	//start communication
 	while (1)
 	{
-		printf("Enter message : ");
-		cin >> message;
+		
+		printf("\n Entrez le nom du fichier complet à téléverser : ");
+		cin >> FileName;
+
+		if (FileName == "validation.txt") {
+			printf("\n Le fichier text est pret a etre envoyer ");
+			inFile.open("C:\\Users\\barm1815\\APP6\\validation.txt");
+			if (!inFile) {
+				cerr << "Unable to open file datafile.txt";
+				exit(1);   // call system to stop
+			}
+			while (!inFile.eof()) {
+				inFile >> FileContent;
+				cout << FileContent;
+			}
+			inFile.close();
+		}
+		else {
+			printf("\n Erreur mauvais nom de fichier");
+		}
+
+		printf("\n Entrer ladresse ip du destinataire (127.0.0.1) ");
+		cin >> ipAdress;
+
+		sendFile(FileContent, File_Length);
+
+		//printf("\n Entrez le message : ");
+		//cin >> message;
 
 		//send the message
-		if (sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
+		/*if (sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
 		{
 			printf("sendto() failed with error code : %d", WSAGetLastError());
 			exit(EXIT_FAILURE);
-		}
+		}*/
 
 		//receive a reply and print it
 		//clear the buffer by filling null, it might have previously received data
-		memset(buf, '\0', BUFLEN);
+		//memset(buf, '\0', BUFLEN);
 		//try to receive some data, this is a blocking call
-		if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == SOCKET_ERROR)
+		/*if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == SOCKET_ERROR)
 		{
 			printf("recvfrom() failed with error code : %d", WSAGetLastError());
 			exit(EXIT_FAILURE);
 		}
 
-		puts(buf);
+		cout << buf;*/
 	}
 
 	closesocket(s);
